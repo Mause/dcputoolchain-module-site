98ec8b46987a0cd319f33e367b0f817da4f454c3
Dominic May <jack.thatch@gmail.com>
2012-05-21 18:37:49 +0800

    Corrected last commit

--- main.py
+++ main.py
@@ -90,7 +90,7 @@ class ListModulesHandler(webapp2.RequestHandler):
             cursor.execute('SELECT * FROM FILES')
             data = cursor.fetchall()
             for x in data:
-                if x != '': self.response.write(str(x[1]))
+                if x != '': self.response.write(str(x[1])+'\n')
         except sqlite3.Error, e:
             print "Error %s:" % e.args[0]
         finally:
