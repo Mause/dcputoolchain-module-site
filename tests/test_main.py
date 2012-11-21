3dd76b5e1d27b7ffe10beb3f4967c381192372f9
Dominic May <jack.thatch@gmail.com>
2012-11-21 18:36:49 +0800

    Fixed?

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,7 @@
 # import json
 # import base64
 import unittest2
-# from mock import patch
+from mock import patch
 if __name__ == '__main__':
     from run_tests import setup_environ
     setup_environ()
@@ -19,8 +19,6 @@ class Test_Main(unittest2.TestCase):
         self.testbed.init_urlfetch_stub()
 
     def test_SearchModuleHandler(self):
-        # from webob import exc
-
         class MockRequestHandler:
             html_output = ''
 
@@ -31,12 +29,25 @@ class Test_Main(unittest2.TestCase):
                 def write(string):
                     self.html_output += str(string)
 
+        # def get_url_content(handler=None, url=None):
+        #     return {'content': base64.b64encode('''
+        #                 HARDWARE = {
+        #                     ID = 0x74fa4cae,
+        #                     Version = 0x07c2,
+        #                     Manufacturer = 0x21544948 -- HAROLD_IT
+        #                 };''')}
+
+        # patcher = patch(
+        #     'dtmm_utils.get_url_content', get_url_content)
+        # self.addCleanup(patcher.stop)
+        # patcher.start()
+
         # from tidylib import tidy_document
 
         import main
         print dir(main)
 
-        html_output = main.SearchModulesHandler().get(MockRequestHandler())
+        html_output = main.SearchModulesHandler.get(MockRequestHandler())
         print html_output
         # document, errors = tidy_document(html_output)
         # self.assertEqual()
