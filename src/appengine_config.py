e65118e722e04dda9f2086665ce930d85eea836c
Dominic May <jack.thatch@gmail.com>
2012-11-21 23:45:14 +0800

    Corrected django version

--- src/appengine_config.py
+++ src/appengine_config.py
@@ -1,11 +1,11 @@
 import os
 from google.appengine.dist import use_library
 os.environ['DJANGO_SETTINGS_MODULE'] = 'settings'
-use_library('django', '1.4.2.final.0')
+use_library('django', '1.3')
 
 
 appstats_CALC_RPC_COSTS = True
-webapp_django_version = '1.4.2.final.0'
+webapp_django_version = '1.3'
 
 
 def webapp_add_wsgi_middleware(app):
