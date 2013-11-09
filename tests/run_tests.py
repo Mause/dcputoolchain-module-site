84cbd9f60713cb754d9db5cf8116d38df7070470
Dominic May <jack.thatch@gmail.com>
2013-11-09 21:36:56 +0800

    Register module_utils tests

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -41,7 +41,12 @@ setup_environ()
 import unittest2
 
 # unit test subunits
-SUB_UNITS = ['test_humans', 'test_dtmm_utils', 'test_handlers']
+SUB_UNITS = [
+    'test_humans',
+    'test_dtmm_utils',
+    'test_handlers',
+    'test_module_utils'
+]
 
 
 def main():
