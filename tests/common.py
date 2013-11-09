ebce0a1492285df07b88fdaaddede1b87dc425fc
Dominic May <jack.thatch@gmail.com>
2013-11-10 01:51:39 +0800

    Added some module test data

--- tests/common.py
+++ tests/common.py
@@ -22,6 +22,20 @@ TEST_GET_TREE = TEST_HANDLERS_GET_TREE = [{
     u'size': 823
 }]
 
+DATA_TREE_DATA = '''
+MODULE = {
+    Type = "Hardware",
+    Name = "HMD2043",
+    Version = "1.1",
+    SDescription = "Deprecated HMD2043 hardware device",
+    URL = "False URL"
+};
+HARDWARE = {
+    ID = 0x74fa4cae,
+    Version = 0x07c2,
+    Manufacturer = 0x21544948 -- HAROLD_IT
+};'''
+
 
 import unittest2
 from google.appengine.ext import testbed
