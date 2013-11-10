f5d19ce12c8217e7daa485339938e89cf3b69c3a
Dominic May <jack.thatch@gmail.com>
2013-11-10 16:37:59 +0800

    Compile Re's at startup

--- src/module_utils.py
+++ src/module_utils.py
@@ -1,9 +1,12 @@
 import re
 from slpp import slpp as lua
 
+HARDWARE_RE = re.compile(r'HARDWARE\s*=\s*(?P<data>\{[^}]*\})')
+MODULE_RE = re.compile(r'MODULE\s*=\s*(?P<data>\{[^}]*\})')
 
-def generic_get_module_data(module, regex):
-    data = re.search(regex, module)
+
+def generic_get_module_data(module, REGEX):
+    data = REGEX.search(module)
 
     return lua.decode(data.groupdict()['data']) if data else {}
 
@@ -11,10 +14,10 @@ def generic_get_module_data(module, regex):
 def get_hardware_data(data):
     """Given a get_tree fragment,
     returns hardware data in a python dict"""
-    return generic_get_module_data(data, r'HARDWARE\s*=\s*(?P<data>\{[^}]*\})')
+    return generic_get_module_data(data, HARDWARE_RE)
 
 
 def get_module_data(data):
     """Given a get_tree fragment,
     returns module data in a python dict"""
-    return generic_get_module_data(data, r'MODULE\s*=\s*(?P<data>\{[^}]*\})')
+    return generic_get_module_data(data, MODULE_RE)
