05179c428653eac3b139d824e6b7a5d6c0cf8f3e
Dominic May <jack.thatch@gmail.com>
2013-11-10 22:26:34 +0800

    Split appart pretty_tree tests

--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -40,13 +40,14 @@ class TestHandlers(common.DMSTestCase):
 
     @patch('dtmm_utils.get_modules')
     @patch('dtmm_utils._get_live_data', lambda handler, fragment: common.DATA_TREE_DATA)
-    def test_human_tree_pretty(self, get_modules):
+    def test_human_tree_pretty_single_module(self, get_modules):
         get_modules.return_value = [{'path': 'assert.lua'}]
 
         self.testapp.get('/human/tree/pretty')
-        get_modules.reset_mock()
-        memcache.flush_all()
 
+    @patch('dtmm_utils.get_modules')
+    @patch('dtmm_utils._get_live_data', lambda handler, fragment: common.DATA_TREE_DATA)
+    def test_human_tree_pretty_rows(self, get_modules):
         get_modules.return_value = [
             {'path': 'assert.lua'},
             {'path': 'assertdb.lua'},
@@ -57,8 +58,9 @@ class TestHandlers(common.DMSTestCase):
 
         self.testapp.get('/human/tree/pretty')
 
-        get_modules.reset_mock()
-        memcache.flush_all()
+    @patch('dtmm_utils.get_modules')
+    @patch('dtmm_utils._get_live_data', lambda handler, fragment: common.DATA_TREE_DATA)
+    def test_human_tree_pretty_single_row(self, get_modules):
 
         get_modules.return_value = [
             {'path': 'assert.lua'},
@@ -67,7 +69,55 @@ class TestHandlers(common.DMSTestCase):
         ]
         self.testapp.get('/human/tree/pretty')
 
-        # intentional double-up
+    def test_human_tree_pretty_memcache(self):
+        memcache.set(
+            'pretty_tree_tree',
+            [
+                {
+                    u'index': 0,
+                    'Version': '1.1',
+                    'Name': 'HMD2043',
+                    'URL': 'False URL',
+                    u'filename': u'assert.lua',
+                    u'width': 300.0,
+                    'SDescription': 'Deprecated HMD2043 hardware device',
+                    'Type': 'Hardware',
+                    u'row': False
+                },
+                {
+                    u'index': 1,
+                    'Version': '1.1',
+                    'Name': 'HMD2043',
+                    'URL': 'False URL',
+                    u'filename': u'assertdb.lua',
+                    u'width': 300.0,
+                    'SDescription': 'Deprecated HMD2043 hardware device',
+                    'Type': 'Hardware', u'row': False
+                },
+                {
+                    u'index': 2,
+                    'Version': '1.1',
+                    'Name': 'HMD2043',
+                    'URL': 'False URL',
+                    u'filename': u'assertpp.lua',
+                    u'width': 300.0,
+                    'SDescription': 'Deprecated HMD2043 hardware device',
+                    'Type': 'Hardware', u'row': False
+                }
+            ]
+        )
+        memcache.set(
+            'pretty_tree_calc',
+            {
+                u'cell_height': 80,
+                u'height': 100,
+                u'width': 900,
+                u'outer_container_height': 100,
+                u'margin_height': 50.0,
+                u'margin_width': 450.0
+            }
+        )
+
         self.testapp.get('/human/tree/pretty')
 
     @patch('dtmm_utils.get_tree')
