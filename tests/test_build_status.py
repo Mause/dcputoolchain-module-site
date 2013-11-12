e7eb5120dcb71192347e663da43c7b43dba9e44b
Dominic May <jack.thatch@gmail.com>
2013-11-12 14:53:16 +0800

    Added new build status tests

---
+++ tests/test_build_status.py
@@ -0,0 +1,42 @@
+import common
+import test_data
+
+import json
+from mock import patch, DEFAULT, Mock
+
+from google.appengine.api import memcache
+
+
+@patch('dtmm_utils.get_url_content', autospec=True)
+class TestBuildStatus(common.DMSHandlerTestCase):
+    def test_build_status_passing(self, get_url_content):
+        for platform, url in test_data.PLATFORM_W_URLS:
+            get_url_content.return_value = {'-1': {'text': 'successful'}}
+            self.testapp.get(url)
+
+            self.assertEqualMemcache('build_status_' + platform, 'passing')
+
+    def test_build_status_failing(self, get_url_content):
+        for platform, url in test_data.PLATFORM_W_URLS:
+
+            get_url_content.return_value = {'-1': {'text': 'failed'}}
+            self.testapp.get(url)
+
+            self.assertEqualMemcache('build_status_' + platform, 'failing')
+
+    def test_build_status_unknown(self, get_url_content):
+        for platform, url in test_data.PLATFORM_W_URLS:
+            get_url_content.return_value = []
+            get_url_content.side_effect = lambda *args: DEFAULT
+            self.testapp.get(url)
+
+            self.assertEqualMemcache('build_status_' + platform, 'unknown')
+
+    def test_build_status_invalid_platfrom(self, _):
+        self.testapp.get('/status/invalid.png')
+
+        values = memcache.get_multi(test_data.PLATFORMS, 'build_status_')
+        self.assertTrue(not any(values))
+
+if __name__ == '__main__':
+    common.main()
