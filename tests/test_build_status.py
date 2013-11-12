0446254a68fd414a82901db930b5e2f00d0eb271
Dominic May <jack.thatch@gmail.com>
2013-11-13 00:32:49 +0800

    Patch notify_status, use that to ensure correct behaviour

--- tests/test_build_status.py
+++ tests/test_build_status.py
@@ -1,52 +1,54 @@
 import common
 import test_data
 
-from mock import patch
+from mock import patch, ANY, MagicMock
 
 from google.appengine.api import memcache
 
 
 @patch('dtmm_utils.get_url_content', autospec=True)
+@patch('build_status.BuildStatusHandler.notify_status', autospec=True)
 class TestBuildStatus(common.DMSHandlerTestCase):
-    def test_build_status_passing(self, get_url_content):
+    def test_build_status_passing(self, notify_status, get_url_content):
         for platform, url in test_data.PLATFORM_W_URLS:
             get_url_content.return_value = {'-1': {'text': 'successful'}}
             self.testapp.get(url)
 
-            self.assertEqualMemcache('build_status_' + platform, 'passing')
+            notify_status.assert_called_with(ANY, 'passing')
 
-    def test_build_status_failing(self, get_url_content):
+    def test_build_status_failing(self, notify_status, get_url_content):
         for platform, url in test_data.PLATFORM_W_URLS:
 
             get_url_content.return_value = {'-1': {'text': 'failed'}}
             self.testapp.get(url)
 
-            self.assertEqualMemcache('build_status_' + platform, 'failing')
+            notify_status.assert_called_with(ANY, 'failing')
 
-    def test_build_status_unknown_with_bad_data(self, get_url_content):
+    def test_build_status_unknown_with_bad_data(self, notify_status, get_url_content):
         for platform, url in test_data.PLATFORM_W_URLS:
             get_url_content.return_value = []
             self.testapp.get(url)
 
-            self.assertEqualMemcache('build_status_' + platform, 'unknown')
+            notify_status.assert_called_with(ANY, 'unknown')
 
-    def test_build_status_unknown_with_no_status(self, get_url_content):
+    def test_build_status_unknown_with_no_status(self, notify_status, get_url_content):
         for platform, url in test_data.PLATFORM_W_URLS:
             get_url_content.return_value = {'-1': {'text': 'garbage'}}
             self.testapp.get(url)
 
-            self.assertEqualMemcache('build_status_' + platform, 'unknown')
+            notify_status.assert_called_with(ANY, 'unknown')
 
-    def test_build_status_invalid_platfrom(self, _):
+    def test_build_status_invalid_platfrom(self, notify_status, _):
         self.testapp.get('/status/invalid.png')
 
         values = memcache.get_multi(test_data.PLATFORMS, 'build_status_')
         self.assertTrue(not any(values))
 
-    def test_build_status_with_memcache(self, _):
+    def test_build_status_with_memcache(self, notify_status, _):
         memcache.set('build_status_linux', 'unknown')
         self.testapp.get('/status/linux.png')
 
+        notify_status.assert_called_with(ANY, 'unknown')
 
 if __name__ == '__main__':
     common.main()
